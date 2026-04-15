632125116c8ee849e8cff3d1054c1351e57b9a58
maria <mariafernandezj24@gmail.com>
2026-04-15 22:12:54 +0200

    format

--- guipanel.cpp
+++ guipanel.cpp
@@ -1,37 +1,39 @@
 
 #include "guipanel.h"
 #include "ui_guipanel.h"
-#include <QSerialPort>      // Comunicacion por el puerto serie
-#include <QSerialPortInfo>  // Comunicacion por el puerto serie
+#include <QSerialPort>     // Comunicacion por el puerto serie
+#include <QSerialPortInfo> // Comunicacion por el puerto serie
 
-#include<stdint.h>      // Cabecera para usar tipos de enteros con tamaño
-#include<stdbool.h>     // Cabecera para usar booleanos
+#include <stdint.h>  // Cabecera para usar tipos de enteros con tamaño
+#include <stdbool.h> // Cabecera para usar booleanos
 
-extern "C" {
-#include "serial2USBprotocol.h"    // Cabecera de funciones de gestión de tramas; se indica que está en C, ya que QTs
-// se integra en C++, y el C puede dar problemas si no se indica.
+extern "C"
+{
+#include "serial2USBprotocol.h" // Cabecera de funciones de gestión de tramas; se indica que está en C, ya que QTs
+    // se integra en C++, y el C puede dar problemas si no se indica.
 }
 
 #include "usb_messages_table.h"
 
-GUIPanel::GUIPanel(QWidget *parent) :  // Constructor de la clase
-    QWidget(parent),
-    ui(new Ui::GUIPanel)               // Indica que guipanel.ui es el interfaz grafico de la clase
-    , transactionCount(0)
+GUIPanel::GUIPanel(QWidget *parent) : // Constructor de la clase
+                                      QWidget(parent),
+                                      ui(new Ui::GUIPanel) // Indica que guipanel.ui es el interfaz grafico de la clase
+                                      ,
+                                      transactionCount(0)
 {
-    ui->setupUi(this);                // Conecta la clase con su interfaz gráfico.
+    ui->setupUi(this);                                      // Conecta la clase con su interfaz gráfico.
     setWindowTitle(tr("Fabrica Automatizada (2025/2026)")); // Título de la ventana
 
     // Conexion por el puerto serie-USB
-    fConnected=false;                 // Todavía no hemos establecido la conexión USB
+    fConnected = false;              // Todavía no hemos establecido la conexión USB
     ui->serialPortComboBox->clear(); // Vacía de componentes la comboBox
     foreach (const QSerialPortInfo &info, QSerialPortInfo::availablePorts())
         // La identificación nos permite que SOLO aparezcan los interfaces tipo USB serial de Texas Instrument
-        if ((info.vendorIdentifier()==0x1CBE) && (info.productIdentifier()==0x0002))
+        if ((info.vendorIdentifier() == 0x1CBE) && (info.productIdentifier() == 0x0002))
         {
             ui->serialPortComboBox->addItem(info.portName());
         }
-    ui->serialPortComboBox->setFocus();   // Componente del GUI seleccionado de inicio
+    ui->serialPortComboBox->setFocus(); // Componente del GUI seleccionado de inicio
     // Las funciones CONNECT son la base del funcionamiento de QT; conectan dos componentes
     // o elementos del sistema; uno que GENERA UNA SEÑAL; y otro que EJECUTA UNA FUNCION (SLOT) al recibir dicha señal.
     // En el ejemplo se conecta la señal readyRead(), que envía el componente que controla el puerto USB serie (serial),
@@ -41,30 +43,29 @@ GUIPanel::GUIPanel(QWidget *parent) :  // Constructor de la clase
     // del programador
     connect(&serial, SIGNAL(readyRead()), this, SLOT(readRequest()));
 
-    ui->pingButton->setEnabled(false);    // Se deshabilita el botón de ping del interfaz gráfico, hasta que
+    ui->pingButton->setEnabled(false); // Se deshabilita el botón de ping del interfaz gráfico, hasta que
     // se haya establecido conexión
 
-    //Inicializa la ventana pop-up PING
+    // Inicializa la ventana pop-up PING
     ventanaPopUp.setIcon(QMessageBox::Information);
     ventanaPopUp.setText(tr("Status: RESPUESTA A PING RECIBIDA"));
     ventanaPopUp.setStandardButtons(QMessageBox::Ok);
     ventanaPopUp.setWindowTitle(tr("Evento"));
-    ventanaPopUp.setParent(this,Qt::Popup);
+    ventanaPopUp.setParent(this, Qt::Popup);
 }
 
 GUIPanel::~GUIPanel() // Destructor de la clase
 {
-    delete ui;   // Borra el interfaz gráfico asociado a la clase
+    delete ui; // Borra el interfaz gráfico asociado a la clase
 }
 
 void GUIPanel::readRequest()
 {
-    int StopCharPosition,StartCharPosition,tam;   // Solo uso notacin hungara en los elementos que se van a
+    int StopCharPosition, StartCharPosition, tam; // Solo uso notacin hungara en los elementos que se van a
     // intercambiar con el micro - para control de tamaño -
     uint8_t *pui8Frame; // Puntero a zona de memoria donde reside la trama recibida
     void *ptrtoparam;
     uint8_t ui8Message; // Para almacenar el mensaje de la trama entrante
-
 
     incommingDataBuffer.append(serial.readAll()); // Añade el contenido del puerto serie USB al array de bytes 'incommingDataBuffer'
     // así vamos acumulando  en el array la información que va llegando
@@ -72,39 +73,40 @@ void GUIPanel::readRequest()
     // Busca la posición del primer byte de fin de trama (0xFD) en el array. Si no estuviera presente,
     // salimos de la funcion, en caso contrario, es que ha llegado al menos una trama.
     // Hay que tener en cuenta que pueden haber llegado varios paquetes juntos.
-    StopCharPosition=incommingDataBuffer.indexOf((char)STOP_FRAME_CHAR,0);
-    while (StopCharPosition>=0)
+    StopCharPosition = incommingDataBuffer.indexOf((char)STOP_FRAME_CHAR, 0);
+    while (StopCharPosition >= 0)
     {
-        //Ahora buscamos el caracter de inicio correspondiente.
-        StartCharPosition=incommingDataBuffer.lastIndexOf((char)START_FRAME_CHAR,0); //Este seria el primer caracter de inicio que va delante...
+        // Ahora buscamos el caracter de inicio correspondiente.
+        StartCharPosition = incommingDataBuffer.lastIndexOf((char)START_FRAME_CHAR, 0); // Este seria el primer caracter de inicio que va delante...
 
-        if (StartCharPosition<0)
+        if (StartCharPosition < 0)
         {
-            //En caso de que no lo encuentre, no debo de hacer nada, pero debo vaciar las primeras posiciones hasta STOP_FRAME_CHAR (inclusive)
-            incommingDataBuffer.remove(0,StopCharPosition+1);
-            LastError=QString("Status:Fallo trozo paquete recibido");
-        } else
+            // En caso de que no lo encuentre, no debo de hacer nada, pero debo vaciar las primeras posiciones hasta STOP_FRAME_CHAR (inclusive)
+            incommingDataBuffer.remove(0, StopCharPosition + 1);
+            LastError = QString("Status:Fallo trozo paquete recibido");
+        }
+        else
         {
-            incommingDataBuffer.remove(0,StartCharPosition); //Si hay datos anteriores al caracter de inicio, son un trozo de trama incompleto. Los tiro.
-            tam=StopCharPosition-StartCharPosition+1;//El tamanio de la trama es el numero de bytes desde inicio hasta fin, ambos inclusive.
-            if (tam>=MINIMUM_FRAME_SIZE)
+            incommingDataBuffer.remove(0, StartCharPosition); // Si hay datos anteriores al caracter de inicio, son un trozo de trama incompleto. Los tiro.
+            tam = StopCharPosition - StartCharPosition + 1;   // El tamanio de la trama es el numero de bytes desde inicio hasta fin, ambos inclusive.
+            if (tam >= MINIMUM_FRAME_SIZE)
             {
-                pui8Frame=(uint8_t*)incommingDataBuffer.data(); // Puntero de trama al inicio del array de bytes
-                pui8Frame++; //Nos saltamos el caracter de inicio.
-                tam-=2; //Descontamos los bytes de inicio y fin del tamanio del paquete
+                pui8Frame = (uint8_t *)incommingDataBuffer.data(); // Puntero de trama al inicio del array de bytes
+                pui8Frame++;                                       // Nos saltamos el caracter de inicio.
+                tam -= 2;                                          // Descontamos los bytes de inicio y fin del tamanio del paquete
 
                 // Paso 1: Destuffing y cálculo del CRC. Si todo va bien, obtengo la trama
                 // con valores actualizados y sin bytes de CRC.
-                tam=destuff_and_check_checksum((unsigned char *)pui8Frame,tam);
-                if (tam>=0)
+                tam = destuff_and_check_checksum((unsigned char *)pui8Frame, tam);
+                if (tam >= 0)
                 {
-                    //El paquete está bien, luego procedo a tratarlo.
-                    ui8Message=decode_message_type(pui8Frame); // Obtencion del byte de Mensaje
-                    tam=get_message_param_pointer(pui8Frame,tam,&ptrtoparam);
-                    switch(ui8Message) // Segun el mensaje tengo que hacer cosas distintas
+                    // El paquete está bien, luego procedo a tratarlo.
+                    ui8Message = decode_message_type(pui8Frame); // Obtencion del byte de Mensaje
+                    tam = get_message_param_pointer(pui8Frame, tam, &ptrtoparam);
+                    switch (ui8Message) // Segun el mensaje tengo que hacer cosas distintas
                     {
                     /** A PARTIR AQUI ES DONDE SE DEBEN AÑADIR NUEVAS RESPUESTAS ANTE LOS MENSAJES QUE SE ENVIEN DESDE LA TIVA **/
-                    case MENSAJE_PING:  // Algunos mensajes no tiene parametros
+                    case MENSAJE_PING: // Algunos mensajes no tiene parametros
                         // Crea una ventana popup con el texto indicado
                         pingResponseReceived();
                         break;
@@ -114,7 +116,7 @@ void GUIPanel::readRequest()
                         // En otros mensajes hay que extraer los parametros de la trama y copiarlos
                         // a una estructura para poder procesar su informacion
                         PARAM_MENSAJE_NO_IMPLEMENTADO parametro;
-                        if (check_and_extract_message_param(ptrtoparam, tam, sizeof(parametro),&parametro)>0)
+                        if (check_and_extract_message_param(ptrtoparam, tam, sizeof(parametro), &parametro) > 0)
                         {
                             // Muestra en una etiqueta (statuslabel) del GUI el mensaje
                             ui->statusLabel->setText(tr("  Mensaje rechazado,"));
@@ -126,8 +128,8 @@ void GUIPanel::readRequest()
                     }
                     break;
 
-                        //Falta por implementar la recepcion de mas tipos de mensajes
-                        //habria que decodificarlos y emitir las señales correspondientes con los parametros que correspondan
+                        // Falta por implementar la recepcion de mas tipos de mensajes
+                        // habria que decodificarlos y emitir las señales correspondientes con los parametros que correspondan
 
                         /*case MENSAJE_CONTADOR:
                     {
@@ -165,12 +167,11 @@ void GUIPanel::readRequest()
 
                             ui->led->setChecked(!ui->led->isChecked());
 
-                             ui->statusLabel->setText(
+                            ui->statusLabel->setText(
                                 tr("Kit %1 Prod %2 Cont %3")
                                     .arg(parametro.kit_id)
                                     .arg(parametro.IDProd)
-                                    .arg(parametro.contador)
-                                );
+                                    .arg(parametro.contador));
                             ui->lcdProd->display((int)parametro.IDProd);
                         }
                         else
@@ -181,15 +182,15 @@ void GUIPanel::readRequest()
                     break;
 
                     default:
-                        //Este error lo notifico mediante la señal statusChanged
-                        LastError=QString("Status: Recibido paquete inesperado");
+                        // Este error lo notifico mediante la señal statusChanged
+                        LastError = QString("Status: Recibido paquete inesperado");
                         ui->statusLabel->setText(tr("  Recibido paquete inesperado,"));
                         break;
                     }
                 }
                 else
                 {
-                    LastError=QString("Status: Error de stuffing o CRC");
+                    LastError = QString("Status: Error de stuffing o CRC");
                     ui->statusLabel->setText(tr(" Error de stuffing o CRC"));
                 }
             }
@@ -197,15 +198,15 @@ void GUIPanel::readRequest()
             {
 
                 // B. La trama no está completa o no tiene el tamano adecuado... no lo procesa
-                //Este error lo notifico mediante la señal statusChanged
-                LastError=QString("Status: Error trozo paquete recibido");
+                // Este error lo notifico mediante la señal statusChanged
+                LastError = QString("Status: Error trozo paquete recibido");
                 ui->statusLabel->setText(tr(" Fallo trozo paquete recibido"));
             }
-            incommingDataBuffer.remove(0,StopCharPosition-StartCharPosition+1); //Elimino el trozo que ya he procesado
+            incommingDataBuffer.remove(0, StopCharPosition - StartCharPosition + 1); // Elimino el trozo que ya he procesado
         }
 
-        StopCharPosition=incommingDataBuffer.indexOf((char)STOP_FRAME_CHAR,0); //Compruebo si el se ha recibido alguna trama completa mas. (Para ver si tengo que salir del bucle o no
-    } //Fin del while....
+        StopCharPosition = incommingDataBuffer.indexOf((char)STOP_FRAME_CHAR, 0); // Compruebo si el se ha recibido alguna trama completa mas. (Para ver si tengo que salir del bucle o no
+    } // Fin del while....
 }
 
 // Funciones auxiliares a la gestión comunicación USB
@@ -215,43 +216,56 @@ void GUIPanel::readRequest()
 // 'serial' que es el que gestiona la comunicación USB serie en el interfaz QT
 void GUIPanel::startSlave()
 {
-    if (serial.portName() != ui->serialPortComboBox->currentText()) {
+    if (serial.portName() != ui->serialPortComboBox->currentText())
+    {
         serial.close();
         serial.setPortName(ui->serialPortComboBox->currentText());
 
-        if (!serial.open(QIODevice::ReadWrite)) {
+        if (!serial.open(QIODevice::ReadWrite))
+        {
             processError(tr("No puedo abrir el puerto %1, error code %2")
-                             .arg(serial.portName()).arg(serial.error()));
+                             .arg(serial.portName())
+                             .arg(serial.error()));
             return;
         }
 
-        if (!serial.setBaudRate(9600)) {
+        if (!serial.setBaudRate(9600))
+        {
             processError(tr("No puedo establecer tasa de 9600bps en el puerto %1, error code %2")
-                             .arg(serial.portName()).arg(serial.error()));
+                             .arg(serial.portName())
+                             .arg(serial.error()));
             return;
         }
 
-        if (!serial.setDataBits(QSerialPort::Data8)) {
+        if (!serial.setDataBits(QSerialPort::Data8))
+        {
             processError(tr("No puedo establecer 8bits de datos en el puerto %1, error code %2")
-                             .arg(serial.portName()).arg(serial.error()));
+                             .arg(serial.portName())
+                             .arg(serial.error()));
             return;
         }
 
-        if (!serial.setParity(QSerialPort::NoParity)) {
+        if (!serial.setParity(QSerialPort::NoParity))
+        {
             processError(tr("NO puedo establecer parida en el puerto %1, error code %2")
-                             .arg(serial.portName()).arg(serial.error()));
+                             .arg(serial.portName())
+                             .arg(serial.error()));
             return;
         }
 
-        if (!serial.setStopBits(QSerialPort::OneStop)) {
+        if (!serial.setStopBits(QSerialPort::OneStop))
+        {
             processError(tr("No puedo establecer 1bitStop en el puerto %1, error code %2")
-                             .arg(serial.portName()).arg(serial.error()));
+                             .arg(serial.portName())
+                             .arg(serial.error()));
             return;
         }
 
-        if (!serial.setFlowControl(QSerialPort::NoFlowControl)) {
+        if (!serial.setFlowControl(QSerialPort::NoFlowControl))
+        {
             processError(tr("No puedo establecer el control de flujo en el puerto %1, error code %2")
-                             .arg(serial.portName()).arg(serial.error()));
+                             .arg(serial.portName())
+                             .arg(serial.error()));
             return;
         }
     }
@@ -267,7 +281,7 @@ void GUIPanel::startSlave()
 
     // Variable indicadora de conexión a TRUE, para que se permita enviar mensajes en respuesta
     // a eventos del interfaz gráfico
-    fConnected=true;
+    fConnected = true;
 }
 
 // Funcion auxiliar de procesamiento de errores de comunicación (usada por startSlave)
@@ -320,9 +334,10 @@ void GUIPanel::pingDevice()
     {
         // El mensaje PING no necesita parametros; de ahí el NULL, y el 0 final.
         // No vamos a usar el mecanismo de numeracion de tramas; pasamos un 0 como n de trama
-        size=create_frame(paquete, MENSAJE_PING, nullptr, 0, MAX_FRAME_SIZE);
+        size = create_frame(paquete, MENSAJE_PING, nullptr, 0, MAX_FRAME_SIZE);
         // Si la trama se creó correctamente, se escribe el paquete por el puerto serie USB
-        if (size>0) serial.write((const char*)paquete,size);
+        if (size > 0)
+            serial.write((const char *)paquete, size);
     }
 }
 
@@ -350,7 +365,7 @@ void GUIPanel::on_pushButton_clicked()
 
         if (size > 0)
         {
-            serial.write((const char*)paquete, size);
+            serial.write((const char *)paquete, size);
             ui->statusLabel->setText("Inicio enviado a TIVA");
         }
         else
@@ -363,4 +378,3 @@ void GUIPanel::on_pushButton_clicked()
         ui->statusLabel->setText("No conectado");
     }
 }
-
